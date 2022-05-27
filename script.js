c9f6afe33e77ce34d7d75bf7ed54d531dafaabe3
dev.esteria04 <67038945+Esteria04@users.noreply.github.com>
2022-05-27 13:52:19 +0000

    started for real?

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
-const root = document.getElementsById("root");
+const root = document.getElementById("root");
 const Workbooks = (workbook) => (
-  <div id="workbooks">
-    <button id={workbook}>{workbook}</button>
-  </div>
+    <button onClick={goToOMR} id={workbook}>{workbook}</button>
 );
+/*
+문제집 선택 버튼 클릭 --> 단원 선택 버튼 클릭 --> omr 입력
+*/
+function goToOMR(workbook) {
+	<div id={"omr_"+workbook}>
+		
+	</div>
+}
 
-ReactDOM.render(Workbooks("XI_STORY",root));
+ReactDOM.render(Workbooks("XI_STORY"),root);
