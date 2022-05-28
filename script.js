7c22f55254bdd35f5e4e888354205067452fe479
dev.esteria04 <67038945+Esteria04@users.noreply.github.com>
2022-05-28 00:45:24 +0000

    idonknow

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
 const root = document.getElementById("root");
 const Workbooks = (workbook) => (
-    <button onClick={goToOMR} id={workbook}>{workbook}</button>
+    <button  id={workbook}>{workbook}</button>
 );
 /*
 문제집 선택 버튼 클릭 --> 단원 선택 버튼 클릭 --> omr 입력
 */
-function goToOMR(workbook) {
-	<div id={"omr_"+workbook}>
-		
-	</div>
-}
 
-ReactDOM.render(Workbooks("XI_STORY"),root);
+const OMRcard = (a) => (
+	<div id={a}>
+		<input id="select-range" placeholder="A01~P34처럼 채점할 범위를 입력해 주세요"></input>
+	</div>
+);
+
+
+ReactDOM.render(Workbooks("2023 자이스토리 수학I"),root);
