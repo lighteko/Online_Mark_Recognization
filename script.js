c24aba990a2d2aff79e2b0004a4fe53e3618fbfd
dev.esteria04 <67038945+Esteria04@users.noreply.github.com>
2022-06-14 14:57:42 +0000

    some things

--- script.js
+++ script.js
@@ -5,83 +5,7 @@ let enterancePageView="block";
 let resultPageView="none";
 let omrCardPageView="none";
 
-let answerBox = [];
-let wrongAns = [];
-const card = document.getElementById("card-place");
-const result = document.getElementById("result-place");
 const root = document.getElementById("root");
-// 단답형 입력값을 최대 세자리 수의 양의 정수로 제한하는 함수
-function checkNum(event) {
-	event.target.value = event.target.value.replace(/[^0-9.]/g, '').replace(/(\..*)\./g, '$1');
-	event.target.value = event.target.value.slice(0,3);
-	if (event.target.value.length >=2 && event.target.value[0]==0) {
-		event.target.value = event.target.value.slice(1,3);
-	}
-	else if (event.target.value.length >=2 && event.target.value[0]==0 && event.target.value[1]==0) {
-		event.target.value = event.target.value.slice(2,3);
-	}
-}
-// 채점 범위를 입력했을 때, 선택한 문제집의 답지 정보를 입력한 범위 만큼만 추출해서 답안 리스트로 만드는 함수
-function answerParser(jsonData, range) {
-	const Range = range.toUpperCase();
-	let keepAppend = false;
-	let appendStarted = false;
-    const [start,end] = Range.split('-');
-	const [[startCh,startNum],[endCh,endNum]] = [start.split('.'),end.split('.')];
-	let parsedAns = [];
-	for (let i = 0; i < jsonData.length+1; i++) {
-		if(startCh == jsonData[i]["Chapter"] && startNum == jsonData[i]["Number"]){
-			keepAppend = true;
-			appendStarted = true;
-		}
-		else if (endCh == jsonData[i]["Chapter"] && endNum == jsonData[i]["Number"]) {
-			keepAppend = false;
-			parsedAns.push(jsonData[i]);
-		}
-		if(keepAppend == true) {
-            parsedAns.push(jsonData[i]);	
-		}
-		if (appendStarted==true && keepAppend==false) {
-			break
-		}
-	answerBox = [];
-	answerBox = parsedAns;
-	}
-    return parsedAns;
-}
-// OMR카드 컴포넌트
-function OMRcard({workbook,display,answers}) {
-	const amount = answers.length; 
-	function OmrCell({Num,Type}) {
-		return (
-		<div id={"omr-block"+Num} style={{display: "flex",marginTop: "10px", marginBottom: "10px"}}>
-			<span style={{width: "50px"}}>{Num}</span>
-			<div id="radios" style={{display:"flex"}}>
-				<Radio name={"omr_cell"+Num} value="1" useAble={Type=="O"? false : true}/>
-				<Radio name={"omr_cell"+Num} value="2" useAble={Type=="O"? false : true}/>
-				<Radio name={"omr_cell"+Num} value="3" useAble={Type=="O"? false : true}/>
-				<Radio name={"omr_cell"+Num} value="4" useAble={Type=="O"? false : true}/>
-				<Radio name={"omr_cell"+Num} value="5" useAble={Type=="O"? false : true}/>
-			</div>
-			<input style={{width:"50px"}} type="text" onInput={checkNum} name={"omr-dir-cell_"+Num} disabled={Type=="O"? true : false} placeholder="단답형"/>
-		</div>
-		);
-	}
-	const OMRblocks = () => {
-		const Blocks = answers.map((answer) => <OmrCell key={answer["Chapter"] + answer["Number"]} Num={answer["Chapter"] + answer["Number"]} Type={answer["Type"]}/>);
-		return (
-			<div id="omr-card">
-			    <div id="omr-blocks">{Blocks}</div>
-			    <button onClick={onSubmitClick} id="submit">채점하기</button>
-			</div>
-		);
-	}
-	return (
-		<div style={{display:display}} id={workbook+"omr-card"}>
-            <OMRblocks/>
-		</div>
-	);	
-}
 
 function Radio({name, value, useAble}) {
 	return(
@@ -110,6 +34,8 @@ function Button({id,cls,value,onclick}) {
 	);
 }
 
+
+
 function Enterance() {	
 	return (
 		<div style={{justifyContent :"center"}} id="enterance">
@@ -130,29 +56,45 @@ function Enterance() {
 	);
 }
 
+
 function Main() {
-	const [step1Load,setStep1Load] = React.useState();
-	const [step2Load,setStep2Load] = React.useState();
-	const [step3Load,setStep3Load] = React.useState();
+	const [step1Load,setStep1Load] = React.useState(true);
+	const [step2Load,setStep2Load] = React.useState(true);
+	const [step3Load,setStep3Load] = React.useState(true);
+    const [range,setRange] = React.useState("");
+
+	const [workbooks,setWorkbooks] = React.useState();
+	React.useEffect(async () => {
+		let workbooksJSON = await (await fetch("./workbooks.json")).json();
+		setWorkbooks(workbooksJSON);
+	},[]);
+	
+	//step1
+	const [subject,setSubject] = React.useState();
 	function Step1() {
 		function Subject({value, className}) {
 			return(
-				<button type="button" className={className} onClick={onSubjectClick}>{value}</button>
-			);
-		}
-		
-		function ChooseSubject() {
-			return(
-				<div id="sb-container" style={{display:"flex",justifyContent:"center",marginTop:"40px",marginBottom:"50px"}}>
-					<Subject value="국어" className="sb korean"/>
-					<Subject value="수학" className="sb math"/>
-					<Subject value="영어" className="sb english"/>
-					<Subject value="과학" className="sb science"/>
-					<Subject value="사회" className="sb sociology"/>
+				<div  className="subject-container" id={"subject-"+className.slice(3,className.length)}>
+					<input type="radio" id={className.slice(3,className.length)} onChange={onChange} className={className} name="subject-radio" value={className.slice(3,className.length)} checked={subject === className.slice(3,className.length)}/>
+					<label htmlFor={className.slice(3,className.length)}>{value}</label>
 				</div>
 			);
 		}
-
+		function ChooseSubject() {
+			return(
+				<div id="sb-container"  style={{display:"flex",justifyContent:"center",marginTop:"40px",marginBottom:"50px"}}>
+	 			   	<Subject value="국어" className="sb korean"/>
+				    <Subject value="수학" className="sb math"/>
+				    <Subject value="영어" className="sb english"/>
+				    <Subject value="과학" className="sb science"/>
+				    <Subject value="사회" className="sb sociology"/>
+	  		  	</div>
+			);
+		}
+		function onChange(e) {
+			setSubject(e.target.value);
+			setStep1Load(false);
+		}
 		return (
 			<div id="step1">
 				<H1 value="STEP1"/>
@@ -162,30 +104,38 @@ function Main() {
 		);
 	}
 
+	//step2
+	const [workbook,setWorkbook] = React.useState("");
+	const [workbookData,setWorkbookData] = React.useState();
 	function Step2() {
-		async function onSelectWb(event){
+		function onSelect(event){
 			let wbIndex = event.target.value;
-			let wbData = await (await fetch(`./workbooks/${wbIndex}.json`)).json();
-			setworkBook(wbData);
-			setwbLoading(false);
+			setWorkbook(wbIndex);
+            fetch(`./workbooks/${wbIndex}.json`)
+	            .then((data) => data.json())
+	            .then((data)=>dataUpdate(data));
+		    function dataUpdate(d) {
+			    setWorkbookData(d);
+			    setStep2Load(false);
+			}	
 		}
 
-		function WbDropdown({options}) {
+		function WbDropdown() {
+			let options = workbooks[subject];
 			return(
-				<div id="wbdropdown-container">
-					<select id="wbdropdown" onChange={(event)=>onSelectWb(event)}>
-						<option id="placeholder" value="">문제집을 선택하세요</option>
-						{options.map((option) => (<option value={option["id"]} key={option["id"]}>{option["value"]}</option>))}
-					</select>
-				</div>
+			<div id="wbdropdown-container">
+				<select id="wbdropdown" onChange={onSelect} value={workbook}>
+					<option id="placeholder" value="">문제집을 선택하세요</option>
+					{options.map((option) => (<option value={option["id"]} key={option["id"]}>{option["value"]}</option>))}
+				</select>
+			</div>
 			);
 		}
-
 		return(
 			<div id="step2">
 				<H1 value="STEP2"/>
 				<P value="문제집 선택하기"/>
-				<WbDropdown options={workbooks[subject]}/>
+				<WbDropdown/>
 			</div>
 		);
 	}
@@ -207,7 +157,7 @@ function Main() {
 				return (
 					<div id="range-chapter-start">
 						<h3 style={{width: "40px"}}>시작</h3>
-						<select onChange={(event)=>setStartChp(event.target.value)} id="start-chapter">
+						<select onChange={(event)=>setStartChp(event.target.value)} id="start-chapter" selected={startChp}>
 							<option id="placeholder" value="">단원 선택</option>
 							{chapters.map((chapter) => (<option id={chapter} key={chapter} value={chapter}>{chapter}</option>))}
 						</select>
@@ -322,11 +272,11 @@ function Main() {
 			<div id="step3">
 				<H1 value="STEP3"/>
 				<P value="범위 선택하기"/>
-				{wbLoading==false ? <ChooseRange/> : null}
+				<ChooseRange/>
 			</div>
 		);
 	}
-
+	
 	return (
 		<div id="main" style={{display: "flex",justifyContent:"center"}}>
 			<div id="container">
@@ -346,6 +296,85 @@ function showMainPage() {
     resultPageView="none";
     omrCardPageView="none";
 	ReactDOM.render(<Main/>,root);
+}
+
+function App() {
+	return <Enterance/>
+}
+
+// 단답형 입력값을 최대 세자리 수의 양의 정수로 제한하는 함수
+function checkNum(event) {
+	event.target.value = event.target.value.replace(/[^0-9.]/g, '').replace(/(\..*)\./g, '$1');
+	event.target.value = event.target.value.slice(0,3);
+	if (event.target.value.length >=2 && event.target.value[0]==0) {
+		event.target.value = event.target.value.slice(1,3);
+	}
+	else if (event.target.value.length >=2 && event.target.value[0]==0 && event.target.value[1]==0) {
+		event.target.value = event.target.value.slice(2,3);
+	}
+}
+
+// 채점 범위를 입력했을 때, 선택한 문제집의 답지 정보를 입력한 범위 만큼만 추출해서 답안 리스트로 만드는 함수
+function answerParser(jsonData, range) {
+	const Range = range.toUpperCase();
+	let keepAppend = false;
+	let appendStarted = false;
+    const [start,end] = Range.split('-');
+	const [[startCh,startNum],[endCh,endNum]] = [start.split('.'),end.split('.')];
+	let parsedAns = [];
+	for (let i = 0; i < jsonData.length+1; i++) {
+		if(startCh == jsonData[i]["Chapter"] && startNum == jsonData[i]["Number"]){
+			keepAppend = true;
+			appendStarted = true;
+		}
+		else if (endCh == jsonData[i]["Chapter"] && endNum == jsonData[i]["Number"]) {
+			keepAppend = false;
+			parsedAns.push(jsonData[i]);
+		}
+		if(keepAppend == true) {
+            parsedAns.push(jsonData[i]);	
+		}
+		if (appendStarted==true && keepAppend==false) {
+			break
+		}
+	answerBox = [];
+	answerBox = parsedAns;
+	}
+    return parsedAns;
+}
+
+// OMR카드 컴포넌트
+function OMRcard({workbook,display,answers}) {
+	const amount = answers.length; 
+	function OmrCell({Num,Type}) {
+		return (
+		<div id={"omr-block"+Num} style={{display: "flex",marginTop: "10px", marginBottom: "10px"}}>
+			<span style={{width: "50px"}}>{Num}</span>
+			<div id="radios" style={{display:"flex"}}>
+				<Radio name={"omr_cell"+Num} value="1" useAble={Type=="O"? false : true}/>
+				<Radio name={"omr_cell"+Num} value="2" useAble={Type=="O"? false : true}/>
+				<Radio name={"omr_cell"+Num} value="3" useAble={Type=="O"? false : true}/>
+				<Radio name={"omr_cell"+Num} value="4" useAble={Type=="O"? false : true}/>
+				<Radio name={"omr_cell"+Num} value="5" useAble={Type=="O"? false : true}/>
+			</div>
+			<input style={{width:"50px"}} type="text" onInput={checkNum} name={"omr-dir-cell_"+Num} disabled={Type=="O"? true : false} placeholder="단답형"/>
+		</div>
+		);
+	}
+	const OMRblocks = () => {
+		const Blocks = answers.map((answer) => <OmrCell key={answer["Chapter"] + answer["Number"]} Num={answer["Chapter"] + answer["Number"]} Type={answer["Type"]}/>);
+		return (
+			<div id="omr-card">
+			    <div id="omr-blocks">{Blocks}</div>
+			    <button onClick={onSubmitClick} id="submit">채점하기</button>
+			</div>
+		);
+	}
+	return (
+		<div style={{display:display}} id={workbook+"omr-card"}>
+            <OMRblocks/>
+		</div>
+	);	
 }
 
 ReactDOM.render(<App/>,root);
